3b4e080169839e9cb808509300efbc34a496d1a5
Aaron Azlant <aaron@destinationsc.co>
2015-08-20 00:24:57 -0700

    first pass to convert react-redux-universal-hot-example to barebones repo to start building todoMVC from
    notes: not happy with the initial reducer structure
    
    known issue: seem to have an issue with hot reloading

--- src/views/App.js
+++ src/views/App.js
@@ -3,14 +3,10 @@ import {Link} from 'react-router';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import DocumentMeta from 'react-document-meta';
-import {isLoaded as isInfoLoaded} from '../reducers/info';
-import {isLoaded as isAuthLoaded} from '../reducers/auth';
-import {load as loadInfo} from '../actions/infoActions';
-import {load as loadAuth, logout} from '../actions/authActions';
-import InfoBar from '../components/InfoBar';
 import {createTransitionHook} from '../universalRouter';
 
-const title = 'React Redux Example';
+
+const title = 'React Redux TodoMVC Example';
 const description = 'All the modern best practices in one example.';
 const image = 'https://react-redux.herokuapp.com/logo.jpg';
 
@@ -38,13 +34,10 @@ const meta = {
 };
 
 @connect(
-    state => ({user: state.auth.user}),
-    dispatch => bindActionCreators({logout}, dispatch))
+    state => ({}),
+    dispatch => bindActionCreators({}, dispatch))
 export default class App extends Component {
   static propTypes = {
-    children: PropTypes.object.isRequired,
-    user: PropTypes.object,
-    logout: PropTypes.func.isRequired
   }
 
   static contextTypes = {
@@ -59,13 +52,7 @@ export default class App extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (!this.props.user && nextProps.user) {
-      // login
-      this.context.router.transitionTo('/loginSuccess');
-    } else if (this.props.user && !nextProps.user) {
-      // logout
       this.context.router.transitionTo('/');
-    }
   }
 
   componentWillUnmount() {
@@ -74,64 +61,30 @@ export default class App extends Component {
   }
 
   render() {
-    const {user} = this.props;
-    const styles = require('./App.scss');
     return (
-      <div className={styles.app}>
+      <div>
         <DocumentMeta {...meta}/>
-        <nav className="navbar navbar-default navbar-fixed-top">
-          <div className="container">
-            <Link to="/" className="navbar-brand">
-              <div className={styles.brand}/>
-              React Redux Example
-            </Link>
-
-            <ul className="nav navbar-nav">
-              <li><Link to="/widgets">Widgets</Link></li>
-              <li><Link to="/survey">Survey</Link></li>
-              <li><Link to="/about">About Us</Link></li>
-              {!user && <li><Link to="/login">Login</Link></li>}
-              {user && <li className="logout-link"><a href="/logout" onClick={::this.handleLogout}>Logout</a></li>}
-            </ul>
-            {user &&
-            <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.name}</strong>.</p>}
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a href="https://github.com/erikras/react-redux-universal-hot-example"
-                   target="_blank" title="View on Github"><i className="fa fa-github"/></a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <div className={styles.appContent}>
+        <div>
           {this.props.children}
-        </div>
-        <InfoBar/>
-
-        <div className="well text-center">
-          Have questions? Ask for help <a
-          href="https://github.com/erikras/react-redux-universal-hot-example/issues"
-          target="_blank">on Github</a> or in the <a
-          href="http://www.reactiflux.com/" target="_blank">#react-redux-universal</a> Slack channel.
         </div>
       </div>
     );
   }
 
-  handleLogout(event) {
-    event.preventDefault();
-    this.props.logout();
-  }
+  // handleLogout(event) {
+  //   event.preventDefault();
+  //   this.props.logout();
+  // }
 
-  static fetchData(store) {
-    const promises = [];
-    if (!isInfoLoaded(store.getState())) {
-      promises.push(store.dispatch(loadInfo()));
-    }
-    if (!isAuthLoaded(store.getState())) {
-      promises.push(store.dispatch(loadAuth()));
-    }
-    return Promise.all(promises);
-  }
+  // static fetchData(store) {
+  //   const promises = [];
+  //   if (!isInfoLoaded(store.getState())) {
+  //     promises.push(store.dispatch(loadInfo()));
+  //   }
+  //   if (!isAuthLoaded(store.getState())) {
+  //     promises.push(store.dispatch(loadAuth()));
+  //   }
+  //   return Promise.all(promises);
+  // }
 }
 
