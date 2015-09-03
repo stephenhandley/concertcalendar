6a7d7043ea960b5f87ea28beecc5a3eb13549bc5
Tom Chen <developer@tomchentw.com>
2015-09-04 00:13:25 +0800

    test(TodoItem): add text test

--- src/components/__tests__/TodoItem-test.js
+++ src/components/__tests__/TodoItem-test.js
@@ -12,19 +12,32 @@ describe('TodoItem', () => {
     complete: false
   };
 
-  it('should render li', () => {
-    const component = TestUtils.renderIntoDocument(
-      <TodoItem
-        todo={mockedTodo}
-        editTodo={_.noop}
-        toggleTodo={_.noop}
-        deleteTodo={_.noop}
-      />
-    );
+  describe('rendering', () => {
+    let component;
 
-    const domEl = TestUtils.findRenderedDOMComponentWithTag(component, 'li');
+    before(() => {
+      component = TestUtils.renderIntoDocument(
+        <TodoItem
+          todo={mockedTodo}
+          editTodo={_.noop}
+          toggleTodo={_.noop}
+          deleteTodo={_.noop}
+        />
+      );
+    });
 
-    expect(domEl).toExist();
+    it('should render li', () => {
+      const liComponent = TestUtils.findRenderedDOMComponentWithTag(component, 'li');
+
+      expect(liComponent).toExist();
+    });
+
+    it('should render text in label', () => {
+      const labelComponent = TestUtils.findRenderedDOMComponentWithTag(component, 'label');
+
+      expect(labelComponent).toExist();
+      expect(React.findDOMNode(labelComponent).textContent).toEqual('abc123');
+    });
   });
 
 });
