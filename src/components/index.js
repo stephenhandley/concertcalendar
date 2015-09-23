c3c533435f475d98d6de4f886ad9102ae32d034a
Aaron Azlant <aaron@gmail.com>
2015-09-22 21:37:24 -0700

    fix: installed expect, karma-sinon, and simon; updated components/index.js

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+/**
+ *  Point of contact for component modules
+ *
+ *  ie: import { CounterButton, InfoBar } from 'components';
+ *
+ */
+
+export Footer from './Footer';
+export Header from './Header';
+export TodoInput from './TodoInput';
+export TodoItem from './TodoItem';
+export TodoList from './TodoList';
